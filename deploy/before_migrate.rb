74b627ae7dad0bbb68fb58b3a48097fa60fc27d8
Hiroyuki Kato <dev.kato@gmail.com>
2013-12-13 16:21:58 +0900

    migrate

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,4 +4,4 @@ require 'fileutils'
 
 FileUtils.chmod_R 0777, "#{release_path}/fuel/app/logs"
 
-# `cd #{release_path}; php ./oil refine migrate`
+`cd #{release_path}; sudo FUEL_ENV=production php ./oil refine migrate`
