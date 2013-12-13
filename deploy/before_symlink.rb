2ba472a1722fade077caa647e751de13001bdc62
Hiroyuki Kato <dev.kato@gmail.com>
2013-12-13 16:41:31 +0900

    nf

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -4,5 +4,6 @@ require 'fileutils'
 
 FileUtils.chmod_R 0777, "#{release_path}/fuel/app/logs"
 
-`echo "CREATE DATABASE fuel_prod;" | mysql -unetprice -hmydbinstance.cl2ngqe4p8v9.ap-northeast-1.rds.amazonaws.com -pnaruhaya`
-`cd #{release_path}; sudo FUEL_ENV=production php ./oil refine migrate`
+system %!echo "CREATE DATABASE fuel_prod;" | mysql -unetprice -hmydbinstance.cl2ngqe4p8v9.ap-northeast-1.rds.amazonaws.com -pnaruhaya!
+system %!cd #{release_path}; sudo FUEL_ENV=production php ./oil refine migrate!
+
