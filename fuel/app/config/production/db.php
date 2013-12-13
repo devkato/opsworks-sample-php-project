1c1f83c4afa37cb87867a0f387355df073670465
Hiroyuki Kato <dev.kato@gmail.com>
2013-12-13 17:00:17 +0900

    nf

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -7,7 +7,7 @@ return array(
 	'default' => array(
     'type' => 'pdo',
 		'connection'  => array(
-			'dsn'        => 'mysql:host=mydbinstance.cl2ngqe4p8v9.ap-northeast-1.rds.amazonaws.com:;dbname=fuel_prod',
+			'dsn'        => 'mysql:host=mydbinstance.cl2ngqe4p8v9.ap-northeast-1.rds.amazonaws.com;dbname=fuel_prod',
 			'username'   => 'netprice',
 			'password'   => 'naruhaya',
 		),
