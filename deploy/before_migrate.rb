05f3241a179d9ae3ec03d281b2f1142eb21211d6
Hiroyuki Kato <dev.kato@gmail.com>
2013-12-13 16:27:10 +0900

    nf

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,4 +4,5 @@ require 'fileutils'
 
 FileUtils.chmod_R 0777, "#{release_path}/fuel/app/logs"
 
+`echo "CREATE DATABASE fuel_prod;" | mysql -unetprice -hmydbinstance.cl2ngqe4p8v9.ap-northeast-1.rds.amazonaws.com -pnaruhaya`
 `cd #{release_path}; sudo FUEL_ENV=production php ./oil refine migrate`
